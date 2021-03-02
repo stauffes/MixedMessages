1c9e06b988b4dda5946757a1045f2a1136a4b780
Scott Stauffer <scott.a.stauffer@gmail.com>
2021-03-02 09:59:24 -0700

    Updated plan in script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+//Plan to have three arrays of the following word types
+//verbs
+//adjectives
+//nouns
+
+//will develop an array of phrases - anywhere that a phrase has the word
+//noun, verb, or adjective, it will be replaced with a random word
+//from the respective array
+//phrases
